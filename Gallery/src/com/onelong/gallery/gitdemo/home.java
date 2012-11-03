bd0abcd9d82144c876e01cd2b0b00191c8e0b883
Michael <vaiyanzi@gmail.com>
2012-11-03 22:03:51 +0800

    and  onCreate method.

--- Gallery/src/com/onelong/gallery/gitdemo/home.java
+++ Gallery/src/com/onelong/gallery/gitdemo/home.java
@@ -4,11 +4,25 @@
 package com.onelong.gallery.gitdemo;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
 
 /**
  * @author vaiyanzi
  *
  */
 public class home extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		return super.onKeyDown(keyCode, event);
+	}
 
 }
