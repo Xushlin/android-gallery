9f269df3544a0286a97880e2811282446fc00204
Michael <vaiyanzi@gmail.com>
2012-11-03 22:06:57 +0800

    add the keydown back send a message "back".

--- Gallery/src/com/onelong/gallery/gitdemo/home.java
+++ Gallery/src/com/onelong/gallery/gitdemo/home.java
@@ -6,6 +6,7 @@ package com.onelong.gallery.gitdemo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.widget.Toast;
 
 /**
  * @author vaiyanzi
@@ -21,6 +22,10 @@ public class home extends Activity {
 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if(keyCode==KeyEvent.KEYCODE_BACK){
+			Toast.makeText(this,"back", Toast.LENGTH_LONG).show();
+			return false;
+		}
 		// TODO Auto-generated method stub
 		return super.onKeyDown(keyCode, event);
 	}
